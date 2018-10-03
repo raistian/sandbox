74374c256b36dcd05c2b49a47f533c64ccbf557b
raistian <raistian@users.noreply.github.com>
2018-10-03 23:16:43 +1000

    need to be completed.

--- misc/jquery_extend_apex.js
+++ misc/jquery_extend_apex.js
@@ -1,9 +1,15 @@
 $.fn.axe = function() {
+
   var editOnRow = function(pId) {
 
     var suffix = '_data_panel'; // Apex appends this to the end of IRR Static ID.
     var rows = $('[id='+pId+suffix+']').find('td.u-tC').closest('tr');
-    
+    rows.each(function(i,row) {
+      $(row).find('td.u-tL').each(function(j,col) {
+        console.log( $(col).text() );
+        $(col).attr('title', 'overriden click').css('cursor','pointer');
+      });
+    });
   } // editOnRow
 
 } // $.fn.axe
