e81b201ce5a730e8f6445554f25428a909fda266
raistian <raistian@users.noreply.github.com>
2018-10-03 22:31:38 +1000

    using function expressions

--- misc/jquery_extend_apex.js
+++ misc/jquery_extend_apex.js
@@ -2,18 +2,11 @@ $.fn.axe = function() {
   var editOnRow = function(pId) {
 
     var suffix = '_data_panel'; // Apex appends this to the end of IRR Static ID.
-    var el = $('[id='+pId+suffix+']').find('td a.clickAnywhere').first();
-    var elUrl = el.attr('href');
+    var rows = $('[id='+pId+suffix+']').find('td.u-tC').closest('tr');
+    
+  } // editOnRow
 
-    var elHeader = $('.u-tL[headers="STEPSNAME"').first();
-    elHeader.attr('title', 'overriden click');
-    elHeader.css('cursor','pointer');
-    elHeader.on('click', function() {
-      location.href = elUrl;
-    });
+} // $.fn.axe
 
-  }
-
-} // end $.fn.axe
-
+// $('.a-IRR-table tr td.u-tC').text();
 // $('.a-IRR-table tr td.u-tL').text();
