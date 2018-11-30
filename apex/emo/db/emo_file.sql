c9948d609be8fdbac4214e3051e2e66e41b7e61a
Andi Christian <raistian@users.noreply.github.com>
2018-11-30 15:31:05 +1100

    updates

--- apex/emo/db/emo_file.sql
+++ apex/emo/db/emo_file.sql
@@ -1,5 +1,5 @@
--- drop table emo_file purge;
-
+drop table emo_file purge
+/
 create table emo_file (
   file_id           number not null
 , file_type         varchar2(30)
@@ -9,8 +9,20 @@ create table emo_file (
 , mime_type         varchar2(255)
 , file_charset      varchar2(128)
 , blob_content      blob
-);
-
-create unique index emo_file_u1 on emo_file (file_id);
-
-
+, creator           varchar2(255)
+, created           date
+, updater           varchar2(255)
+, updated           date
+)
+/
+create unique index emo_file_u1 on emo_file (file_id)
+/
+create or replace editionable trigger emo_file_t1
+  before insert on emo_file
+  for each row
+begin
+  if :new.file_id is null then
+    select to_number(sys_guid(),'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') into :new.file_id from dual;
+  end if;
+end;
+/
